5e7b6ea7f4dd06002502e58f8faf8aae759356a7
arga <18220023@std.stei.itb.ac.id>
2023-12-18 04:18:49 +0700

    add picture and project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
+import profPict from '/public/arga-fix.jpg';
+import react from '/public/React.svg';
+import flutter from '/public/icon_flutter.svg';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-5 m-5">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5 m-5">
       <div className='object-center'>
         Hi, this is Arga&apos;s Personal Web
       </div>
       <div className='flex flex-row items-center mt-5'>
-        <div className='flex flex-col bg-white items-center justify-between h-56 w-56 m-5 p-5 rounded-xl'>
-          <img src="https://images.unsplash.com/photo-1624669240815-815a23372f37?" alt="arga" className="w-24 h-24 rounded-full object-cover">
+        <div className='flex flex-col bg-white items-center justify-between h-80 w-96 m-5 p-5 rounded-xl'>
+          <img src={profPict.src} alt="arga" className="w-36 h-36 rounded-full object-cover object-center-up">
 
           </img>
 
@@ -22,7 +27,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <div className='flex flex-col bg-slate-400 h-56 w-56 text-center justify-between rounded-xl p-5'>
+        <div className='flex flex-col bg-slate-400 h-80 w-96 text-center justify-evenly rounded-xl p-5'>
           <p>
             Final Year System and Information Technology Student. 
           </p>
@@ -36,15 +41,48 @@ export default function Home() {
       </div>
 
       <div className='flex flex-row items-center mb-10'>
-        <div className='bg-white h-56 w-56 text-center rounded-xl mx-5'>
-          Projects
+        <div className='bg-white h-80 w-96 text-center rounded-xl mx-5 p-5'>
+          <a className='font-bold text-2xl'> Projects</a>
+
+          <div className='flex flex-col h-3/5 mt-3 mb-5'>
+            <Link href='https://pokelib.vercel.app/'>
+              <div className='flex flex-row w-full justify-between bg-slate-300 rounded-xl p-2 px-3 mb-3'>
+                <div className='flex flex-col text-start'>
+                  <div className='text-lg font-bold'>PokeLib</div>
+                  <div className='text-xs'>Pokemon Library with Catch Feature</div>
+                </div>
+                <Image
+                  priority
+                  src={react}
+                  height={30}
+                  alt="react"
+                />
+              </div>
+            </Link>
+            
+            <Link href='https://weather-app-lemon-one.vercel.app/'>
+                <div className='flex flex-row w-full justify-between bg-slate-300 rounded-xl p-2 px-3'>
+                    <div className='flex flex-col text-start'>
+                      <div className='text-lg font-bold'>Weather App</div>
+                      <div className='text-xs'>Fetching Weather Data from OpenAPI</div>
+                    </div>
+                  <Image
+                    priority
+                    className='mx-2'
+                    src={flutter}
+                    height={30}
+                    alt = "flutter"
+                  />
+                  </div>
+            </Link>
+          </div> 
         </div>
-        <div className='bg-slate-400 h-56 w-56 rounded-xl'>
+        <div className='bg-slate-400 h-80 w-96 rounded-xl'>
           Experience
         </div>
       </div>
-      <div>
-        Build using Next.js
+      <div className='mb-10'>
+        Built using Next.js
       </div>  
     </main>
   )
